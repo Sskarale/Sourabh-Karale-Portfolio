a43c9ae339ac0b32e98bbdb845ffb78f342095aa
Sskarale <sourabhkarale01@gmail.com>
2025-05-23 14:33:16 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import { HiMenu, HiX } from "react-icons/hi";
+import image from './image.png'
+
 
 const Navbar = () => {
+  // const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="bg-gray-900 text-white p-4 fixed top-0 w-full shadow-md z-50">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Sourabh Karale</h1>
-        <ul className="flex space-x-4">
-          <li><a href="#home" className="hover:text-yellow-400">Home</a></li>
-          <li><a href="#about" className="hover:text-yellow-400">About</a></li>
-          <li><a href="#projects" className="hover:text-yellow-400">Projects</a></li>
-          <li><a href="#skills" className="hover:text-yellow-400">Skills</a></li>
-          <li><a href="#contact" className="hover:text-yellow-400">Contact</a></li>
+      <div className="max-w-6xl mx-auto flex flex-row justify-between items-center">
+        <div className="flex items-center gap-2">
+          <h1 className="text-2xl font-bold md:block hidden">Sourabh Karale</h1>
+          <img
+            src={image}
+            alt="SK Logo"
+            className="w-10 h-10 md:hidden block rounded-full"
+          />
+        </div>
+        <ul className="flex flex-wrap items-center gap-4 overflow-x-auto text-sm md:text-base">
+          <li><a href="#home" className="hover:text-yellow-400 whitespace-nowrap">Home</a></li>
+          <li><a href="#about" className="hover:text-yellow-400 whitespace-nowrap">About</a></li>
+          <li><a href="#projects" className="hover:text-yellow-400 whitespace-nowrap">Projects</a></li>
+          <li><a href="#skills" className="hover:text-yellow-400 whitespace-nowrap">Skills</a></li>
+          <li><a href="#contact" className="hover:text-yellow-400 whitespace-nowrap">Contact</a></li>
         </ul>
       </div>
     </nav>
+
   )
 }
 
